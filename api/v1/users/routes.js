48fdd7862b0c47e40a54499705dce45cc3f6bb45
Subhadeep Mukherjee <subhadeepmukherjee2019@gmail.com>
2025-07-16 11:43:53 +0530

    Profilepage

--- api/v1/users/routes.js
+++ api/v1/users/routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { sendUserBasicInfoController } = require("./controllers");
+const { sendUserBasicInfoController,sendUserDetailsController } = require("./controllers");
 
 const usersRouter = express.Router();
 
 usersRouter.get("/", sendUserBasicInfoController);
+usersRouter.get("/details", sendUserDetailsController);
 
 module.exports = { usersRouter };
